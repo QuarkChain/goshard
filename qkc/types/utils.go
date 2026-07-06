8c3151b48dc1bd0274434df38c80a1084eee230a
blockchaindevsh <shore.cloud@gmail.com>
2026-07-06 20:13:25 +0800

    qkc/types: use crypto hash helper

--- qkc/types/utils.go
+++ qkc/types/utils.go
@@ -1,14 +1,12 @@
 // Copyright 2026-2027, QuarkChain.
 
 // Ported from github.com/QuarkChain/goquarkchain/core/types (byte-compatible).
-// Adaptation: sha3.NewKeccak256() -> crypto.NewKeccakState() (identical Keccak-256 digest).
+// Adaptation: sha3.NewKeccak256() -> crypto.Keccak256Hash() (identical Keccak-256 digest).
 
 package types
 
 import (
-	"hash"
 	"reflect"
-	"sync"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
@@ -22,41 +20,10 @@ func (c *writeCounter) Write(b []byte) (int, error) {
 	return len(b), nil
 }
 
-type hashBuf struct {
-	bytes *[]byte
-	hw    hash.Hash
-}
-
-func newBuf() *hashBuf {
-	return &hashBuf{bytes: new([]byte), hw: crypto.NewKeccakState()}
-}
-
-func (h *hashBuf) reset() {
-	*h.bytes = (*h.bytes)[:0]
-	h.hw.Reset()
-}
-
-func (h *hashBuf) getHash() (hash common.Hash) {
-	h.hw.Write(*h.bytes)
-	h.hw.Sum(hash[:0])
-	return
-}
-
-var (
-	bufPool = sync.Pool{
-		New: func() interface{} { return &hashBuf{bytes: new([]byte), hw: crypto.NewKeccakState()} },
-	}
-)
-
 func serHash(val interface{}, excludeList map[string]bool) (h common.Hash) {
-	buf := bufPool.Get().(*hashBuf)
-	if buf == nil {
-		buf = newBuf()
-	}
-	buf.reset()
-	defer bufPool.Put(buf)
-	if err := serialize.SerializeStructWithout(reflect.ValueOf(val), buf.bytes, excludeList); err != nil {
+	var bytes []byte
+	if err := serialize.SerializeStructWithout(reflect.ValueOf(val), &bytes, excludeList); err != nil {
 		panic(err)
 	}
-	return buf.getHash()
+	return crypto.Keccak256Hash(bytes)
 }
