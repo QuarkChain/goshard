39e48730478e0445d741cd220fafb5671ddc166f
blockchaindevsh <shore.cloud@gmail.com>
2026-07-21 20:15:00 +0800

    qkc/types: defer receipt storage encoding

--- qkc/types/receipt.go
+++ qkc/types/receipt.go
@@ -8,7 +8,6 @@ package types
 import (
 	"fmt"
 	"io"
-	"math"
 	"unsafe"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -54,28 +53,6 @@ type receiptRLP struct {
 	Logs                 []*coretypes.Log
 	ContractAddress      []byte
 	ContractFullShardKey *Uint32
-}
-
-type receiptStorageRLP struct {
-	Status               []byte
-	CumulativeGasUsed    uint64
-	Bloom                Bloom
-	TxHash               common.Hash
-	ContractAddress      account.Recipient
-	ContractFullShardKey uint32
-	Logs                 []*receiptStorageLog
-	GasUsed              uint64
-}
-
-type receiptStorageLog struct {
-	Recipient   account.Recipient
-	Topics      []common.Hash
-	Data        []byte
-	BlockNumber uint64
-	TxHash      common.Hash
-	TxIndex     uint32
-	BlockHash   common.Hash
-	Index       uint32
 }
 
 // NewReceipt creates a barebone transaction receipt.
@@ -201,97 +178,6 @@ func (r *Receipt) Size() common.StorageSize {
 		size += common.StorageSize(len(log.Topics)*common.HashLength + len(log.Data))
 	}
 	return size
-}
-
-// ReceiptForStorage is a wrapper around a Receipt that flattens and parses the
-// entire content of a receipt, as opposed to only the consensus fields originally.
-type ReceiptForStorage Receipt
-
-// EncodeRLP implements rlp.Encoder, and flattens all content fields of a receipt
-// into an RLP stream.
-func (r *ReceiptForStorage) EncodeRLP(w io.Writer) error {
-	status, err := (*Receipt)(r).statusEncoding()
-	if err != nil {
-		return err
-	}
-	enc := &receiptStorageRLP{
-		Status:               status,
-		CumulativeGasUsed:    r.CumulativeGasUsed,
-		Bloom:                r.Bloom,
-		TxHash:               r.TxHash,
-		ContractAddress:      r.ContractAddress,
-		ContractFullShardKey: r.ContractFullShardKey,
-		Logs:                 make([]*receiptStorageLog, len(r.Logs)),
-		GasUsed:              r.GasUsed,
-	}
-	for i, log := range r.Logs {
-		storageLog, err := logForStorage(log)
-		if err != nil {
-			return fmt.Errorf("log %d: %w", i, err)
-		}
-		enc.Logs[i] = storageLog
-	}
-	return rlp.Encode(w, enc)
-}
-
-// DecodeRLP implements rlp.Decoder, and loads both consensus and implementation
-// fields of a receipt from an RLP stream.
-func (r *ReceiptForStorage) DecodeRLP(s *rlp.Stream) error {
-	var dec receiptStorageRLP
-	if err := s.Decode(&dec); err != nil {
-		return err
-	}
-	if err := (*Receipt)(r).setStatus(dec.Status); err != nil {
-		return err
-	}
-	// Assign the consensus fields
-	r.CumulativeGasUsed, r.Bloom = dec.CumulativeGasUsed, dec.Bloom
-	r.Logs = make([]*coretypes.Log, len(dec.Logs))
-	for i, log := range dec.Logs {
-		r.Logs[i] = log.toCoreLog()
-	}
-	// Assign the implementation fields
-	r.TxHash, r.ContractAddress, r.ContractFullShardKey, r.GasUsed = dec.TxHash, dec.ContractAddress,
-		dec.ContractFullShardKey, dec.GasUsed
-	return nil
-}
-
-func logForStorage(log *coretypes.Log) (*receiptStorageLog, error) {
-	if log == nil {
-		return nil, nil
-	}
-	if log.TxIndex > math.MaxUint32 {
-		return nil, fmt.Errorf("log tx index %d exceeds uint32", log.TxIndex)
-	}
-	if log.Index > math.MaxUint32 {
-		return nil, fmt.Errorf("log index %d exceeds uint32", log.Index)
-	}
-	return &receiptStorageLog{
-		Recipient:   log.Address,
-		Topics:      copyHashSlice(log.Topics),
-		Data:        common.CopyBytes(log.Data),
-		BlockNumber: log.BlockNumber,
-		TxHash:      log.TxHash,
-		TxIndex:     uint32(log.TxIndex),
-		BlockHash:   log.BlockHash,
-		Index:       uint32(log.Index),
-	}, nil
-}
-
-func (l *receiptStorageLog) toCoreLog() *coretypes.Log {
-	if l == nil {
-		return nil
-	}
-	return &coretypes.Log{
-		Address:     l.Recipient,
-		Topics:      copyHashSlice(l.Topics),
-		Data:        common.CopyBytes(l.Data),
-		BlockNumber: l.BlockNumber,
-		TxHash:      l.TxHash,
-		TxIndex:     uint(l.TxIndex),
-		BlockHash:   l.BlockHash,
-		Index:       uint(l.Index),
-	}
 }
 
 // Receipts is a wrapper around a Receipt array to implement DerivableList.
