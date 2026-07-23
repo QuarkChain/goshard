76e7d1d2818513f8eed6daffb56396033e83c7bc
blockchaindevsh <shore.cloud@gmail.com>
2026-07-23 19:21:55 +0800

    qkc/types: add logs and receipts (#34)
    * qkc/types: add logs and receipts
    
    * qkc/types: reuse geth bloom filter
    
    * qkc/types: separate cluster receipt log encoding
    
    * qkc/types: restrict receipts to qkc status encoding
    
    * qkc/types: mark receipt trie fields as consensus
    
    * qkc/types: remove unused receipt json generator
    
    * qkc/types: defer receipt storage encoding
    
    * qkc/types: separate receipt wire conversion
    
    * qkc/types: copy cluster receipt status
    
    * qkc/types: validate receipt contract address length
    
    * qkc/types: use receipt-local cluster log indexes

---
+++ qkc/types/bloom9.go
@@ -0,0 +1,28 @@
+// Copyright 2026-2027, QuarkChain.
+
+// Bloom reuses geth's log-bloom implementation; CreateBloom adapts QKC receipts.
+
+package types
+
+import comtypes "github.com/ethereum/go-ethereum/core/types"
+
+type Bloom = comtypes.Bloom
+
+func CreateBloom(receipts Receipts) Bloom {
+	var bloom Bloom
+	for _, receipt := range receipts {
+		if receipt == nil {
+			continue
+		}
+		for _, log := range receipt.Logs {
+			if log == nil {
+				continue
+			}
+			bloom.Add(log.Address.Bytes())
+			for _, topic := range log.Topics {
+				bloom.Add(topic.Bytes())
+			}
+		}
+	}
+	return bloom
+}
