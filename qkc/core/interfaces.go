2b576d44ef9d2b0cb8d7b2c58ff694fe8817af47
ping-ke <molaokp@gmail.com>
2026-09-23 16:38:26 +0800

    update interface

--- qkc/core/interfaces.go
+++ qkc/core/interfaces.go
@@ -21,13 +21,20 @@ type InsertOptions struct {
 	ForceInsert bool
 }
 
+// XShardDepositCursor is the execution layer's view of incoming cross-shard
+// deposits. Root ancestry and eligibility remain owned by ShardCoordinator.
+type XShardDepositCursor interface {
+	GetNextTx() (*types.CrossShardTransactionDeposit, error)
+	GetCursorInfo() *types.XShardTxCursorInfo
+}
+
 // MinorChain owns local minor block execution, persistence and canonical indexes.
 // Its implementation is delivered separately from the Milestone 1 coordinator.
 type MinorChain interface {
 	CurrentBlock() *types.MinorBlock
 	GetBlock(hash common.Hash) *types.MinorBlock
 	GetBlockByNumber(number uint64) *types.MinorBlock
-	InsertBlockWithXShardInput(block *types.MinorBlock, cursor *XShardTxCursor, options InsertOptions) ([]*types.CrossShardTransactionDeposit, error)
+	InsertBlockWithXShardInput(block *types.MinorBlock, cursor XShardDepositCursor, options InsertOptions) ([]*types.CrossShardTransactionDeposit, error)
 	SetCanonicalHead(hash common.Hash) error
 	Stop()
 }
